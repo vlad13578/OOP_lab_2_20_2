85b2b61f388442f79c199848644119aa549e302a
Роман Данилевич <dwordchanel@gmail.com>
2020-05-26 15:53:42 +0300

    Add project files.

---
+++ OOP_lab_2_20_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOP_lab_2_20_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0;
+
+            while (x <= Math.PI)
+            {
+                double y = Math.Tan(x);
+
+                Console.WriteLine("x = {0:f4} y = {1:f4}", x, y);
+
+                x += Math.PI / 20;
+            }
+        }
+    }
+}
